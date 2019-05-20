7b79c63cf824aabab15d9f9f8fb28b7074e08a66
LaDimas <la_dimas@mail.ru>
2019-05-20 16:57:03 +0600

    Signed-off-by: LaDimas <la_dimas@mail.ru>

---
+++ TEST/JavaEE_Test/src/FirstServlet.java
@@ -0,0 +1,12 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class FirstServlet extends javax.servlet.http.HttpServlet {
+    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+    }
+
+    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+    }
+}
